61d2237012b15889edf33cefbec968d005994154
Marek Ovcacik <marek.ovcacik@gmail.com>
2021-07-14 19:48:29 +0200

    Support pagination, support estate age filtering.

--- estates/spiders/sreality.py
+++ estates/spiders/sreality.py
@@ -1,8 +1,21 @@
 import re
+import os
 import json
 import scrapy
+from scrapy import Request
 
 from estates.items import Estate, EstatePicture, Coords
+
+per_page = 200
+min_area = 1
+max_price = 100_000_000
+
+ages = {
+    'day': 2,
+    'all': 1000
+}
+
+max_age_days = ages[os.environ.get('ESTATE_AGE', 'day')]
 
 
 class SrealitySpider(scrapy.Spider):
@@ -11,11 +24,23 @@ class SrealitySpider(scrapy.Spider):
 
     min_items = 0
 
-    min_area = 1
-    per_page = 200
-    max_price = 100_000_000
-    max_age_days=20
-    start_urls = [f'https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1&czk_price_summary_order2=100000%7C{max_price}&estate_age={max_age_days}&locality_region_id=10&no_auction=1&ownership=1&per_page={per_page}&tms=1616701027739&usable_area={min_area}%7C10000000000']
+    def get_url(page):
+        return (
+            "https://www.sreality.cz/api/cs/v2/estates?"
+                "category_main_cb=1&"
+                "category_type_cb=1&"
+                f"czk_price_summary_order2=100000%7C{max_price}&"
+                f"estate_age={max_age_days}&"
+                "locality_region_id=10&"
+                "no_auction=1&"
+                "ownership=1&"
+                f"page={page}&"
+                f"per_page={per_page}&"
+                "tms=1616701027739&"
+                f"usable_area={min_area}%7C10000000000"
+        )
+
+    start_urls = [get_url(1)]
 
     custom_settings = {
         'ROBOTSTXT_OBEY': False
@@ -49,4 +74,8 @@ class SrealitySpider(scrapy.Spider):
                 area = area,
                 pictures = pictures
             )
-            pass
+
+        current_page = response.meta.get('page', 1)
+        if res_obj['result_size'] > per_page * current_page:
+            next_page = current_page + 1
+            yield Request(url=SrealitySpider.get_url(next_page), meta={'page': next_page})
